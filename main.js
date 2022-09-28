1e9ac253da757beb04dc6d543a60cea52485de93
Your Name <you@example.com>
2022-09-28 17:04:55 +0300

    1.Added getLaneCenter() in class Road 2.Added border property in class Road 3.Called translate(); on main.animate()

--- main.js
+++ main.js
@@ -4,13 +4,21 @@ const canvas = document.getElementById("myCanvas");
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
-const car = new Car(100,100,30,50);
+const road = new Road(canvas.width/2, canvas.width*0.9);
+const car = new Car(road.getLaneCenter(1),100,30,50);
 
 animate();
 
 function animate() {
     car.updateControls();
     canvas.height = window.innerHeight;
+
+    //provides the above camera view of the road
+    //makes it look like the road is moving
+    ctx.save();
+    ctx.translate(0,-car.y+canvas.height*0.7);
+    
+    road.draw(ctx);
     car.draw(ctx);
     requestAnimationFrame(animate);
 }
